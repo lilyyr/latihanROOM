058bbf97a339398b982c3d2e0ad851cd297a772a
Lea <c14220168@john.petra.ac.id>
2024-12-06 23:19:23 +0700

    changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.latihanroom"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.latihanroom"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
